3a01c07d28445cce92b258fbb48c425bcb6cb6d2
Steven Pousty <spousty@redhat.com>
2016-04-27 19:15:31 -0700

    new code

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 __author__ = 'spousty'
 
-#import psycopg2
-from bottle import route, run, DEBUG
+import psycopg2
+from bottle import route, run, get, DEBUG
 import os
 
 
@@ -10,24 +10,25 @@ import os
 def index():
 	return "<h1> hello OpenShift Ninja without DB</h1>"
 
-#@route('/db')
-#def dbexample():
-#	print(os.environ.get('POSTGRESQL_USER'))
-#	print("After Env")
-#	try:
-#		conn = psycopg2.connect(database='db', user=os.environ.get('POSTGRESQL_USER'), host=os.environ.get('POSTGRESQL_92_CENTOS7_SERVICE_HOST'), password=os.environ.get('POSTGRESQL_PASSWORD'))
-#	except:
-#		print( os.environ.get('POSTGRESQL_USER') + "  " + os.environ.get('POSTGRESQL_92_CENTOS7_SERVICE_HOST'))
+# since this is a read only talk to the replicas
+@get('/db')
+def dbexample():
+	print(os.environ.get('POSTGRESQL_USER'))
+	print("After Env")
+	try:
+		conn = psycopg2.connect(database='db', user=os.environ.get('POSTGRESQL_USER'), host=os.environ.get('POSTGRESQL_92_CENTOS7_SERVICE_HOST'), password=os.environ.get('POSTGRESQL_PASSWORD'))
+	except:
+		print( os.environ.get('POSTGRESQL_USER') + "  " + os.environ.get('POSTGRESQL_92_CENTOS7_SERVICE_HOST'))
 	
-#	cur = conn.cursor()
-#	cur.execute("""SELECT * from pg_user""")
+	cur = conn.cursor()
+	cur.execute("""SELECT * from pg_user""")
 	
-#	rows = cur.fetchall()
-#	result_string = "<h2>Here are your results: </h2>"
-#	for row in rows:
-#		result_string += "<h3>" + row[0] + "</h3>"
-#
-#	return  result_string
+	rows = cur.fetchall()
+	result_string = "<h2>Here are your results: </h2>"
+	for row in rows:
+		result_string += "<h3>" + row[0] + "</h3>"
+
+	return  result_string
 
 if __name__ == '__main__':
 	run(host='0.0.0.0', port=8080, debug=True)
