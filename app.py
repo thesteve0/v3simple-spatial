128591598ee6213f350409360c887f100819ce3a
Steven Pousty <spousty@redhat.com>
2016-07-19 12:41:34 -0700

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import os
 
 @route('/')
 def index():
-	return "<h1> hello OpenShift Ninja without DB</h1>"
+	return "<h1> Vagrant and VirtualBox is fun</h1>"
 
 if __name__ == '__main__':
 	run(host='0.0.0.0', port=8080, debug=True)
