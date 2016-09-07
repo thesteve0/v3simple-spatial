41afe06e30c289a440e21d1834b38339f8a7a7ad
Steven Pousty <spousty@redhat.com>
2016-09-06 20:06:58 -0700

    Bringing up to date and fixing formatting

--- 1_app.py
+++ 1_app.py
@@ -4,10 +4,10 @@ from bottle import route, run, DEBUG
 import os
 
 
-
 @route('/')
 def index():
-	return "<h1> Vagrant and VirtualBox is fun</h1>"
+    return "<h1> Vagrant and VirtualBox is fun</h1>"
+
 
 if __name__ == '__main__':
-	run(host='0.0.0.0', port=8080, debug=True)
+    run(host='0.0.0.0', port=8080, debug=True)
