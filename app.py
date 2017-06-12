ff1933808ae44e4287a43352047e2b117148b10a
Steven Pousty <spousty@redhat.com>
2017-06-12 02:19:02 -0700

    Create app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from random_words import RandomWords
 
 @route('/')
 def index():
-    return "<h1> hello OpenShift Ninja without DB</h1>"
+    return "<h1> hello Proper Person</h1>"
 
 
 # since this is a read only talk to the replicas
